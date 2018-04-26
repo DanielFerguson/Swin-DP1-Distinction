f9ac3788b2199c04a8cc2cd71d706037138d0f15
danielferguson <thedanielfergusonkid@gmail.com>
2018-04-26 21:07:13 +1000

    Basic Game
    Basic Game Update
    - Characters
    - Folder Structure
    - Movement
    - Food
    -- Generation
    -- Prefabs
    - Basic UI System
    - Scripts
    -- Eating, Moving
    -- Score System

---
+++ Pathways/Assets/Scripts/Eating.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Eating : MonoBehaviour {
+
+    public string Tag;
+    public Text Letters;
+
+    private int Score = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == Tag)
+        {
+            // Destroy GameObject
+            Destroy(other.gameObject);
+
+            // Increment Score
+            Score += 10;
+            Letters.text = "Score: " + Score;
+        }
+    }
+}
---
+++ Pathways/Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float Speed = 1f;
+
+    private void Update()
+    {
+        Vector3 Target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Target.z = transform.position.z;
+
+        transform.position = Vector3.MoveTowards(transform.position, Target, Speed * Time.deltaTime);
+    }
+}

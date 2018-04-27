b58a32159d2796f9e01946fa8734ffd27c2e5aa3
danielferguson <thedanielfergusonkid@gmail.com>
2018-04-27 20:09:51 +1000

    Character Customization
    Created Character Customisation page, yet to hook up all of the functions / options with a creation method for player customisation.

---
+++ Pathways/Assets/Scripts/Player2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour {
+
+    public float playerSpeed = 120f;
+    public float rotateSpeed = 120f;
+
+    private Rigidbody2D sprite;
+
+    private void Start()
+    {
+        // Fetch Rigidbody attached to GameObject
+        sprite = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Rotation
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * rotateSpeed, Space.World);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(new Vector3(0, 0, -1) * Time.deltaTime * rotateSpeed, Space.World);
+
+        // Forward Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            sprite.velocity = transform.up * playerSpeed * Time.deltaTime;
+        }
+        else
+        {
+            sprite.velocity = Vector2.zero;
+        }
+
+        // Backwards 
+        if (Input.GetKey(KeyCode.S))
+        {
+            sprite.velocity = -transform.up * playerSpeed * Time.deltaTime;
+        }
+    }
+}

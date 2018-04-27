8777c840d67664e053526ec6d4ab598229a1c1d8
danielferguson <thedanielfergusonkid@gmail.com>
2018-04-27 17:59:38 +1000

    UI Updates
    Basic navigation is functional, with volume slider implimented, however not functional / needed until audio is implimented.

---
+++ Pathways/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        // Get current scene index, and increment it into Game Scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}

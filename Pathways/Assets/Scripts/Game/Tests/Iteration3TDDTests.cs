ac3c39e27244a0f7edba932855e2be9eb5b815c2
danielferguson <thedanielfergusonkid@gmail.com>
2018-05-27 17:51:33 +1000

    Added TDD Test File

---
+++ Pathways/Assets/Scripts/Game/Tests/Iteration3TDDTests.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+using NUnit.Framework;
+using System.Collections;
+
+public class Iteration3TDDTests
+{
+    [Test]
+    public void TDDInstantiateEnemy()
+    {
+        // Responsibiltiy: Dan
+        Assert.False(false);
+    }
+
+    [Test]
+    public void TDDLoadCorrectAsset()
+    {
+        // Responsibility: Caleb
+        Assert.False(false);
+    }
+
+    [Test]
+    public void TDDMovementAbility()
+    {
+        // Responsibility: Duncan
+        Assert.False(false);
+    }
+}
